bb4896eba86ba7c9674c2269590760e278b33209
sudhaScode <sunriser7995@gmail.com>
2024-02-18 16:51:30 +0530

    Unit testing done

--- magicalarena/src/test/java/com/game/magicalarena/ArenaTest.java
+++ magicalarena/src/test/java/com/game/magicalarena/ArenaTest.java
@@ -1,14 +1,15 @@
 package com.game.magicalarena;
 
+import org.junit.Test;
+
 import com.game.player.Player;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class ArenaTest {
+public class ArenaTest {
 
     @Test
-    void fight() {
+    public void fight() {
         Game arena = new Arena();
 
         Player playerA = new Player("Player A", 50, 5, 10);
@@ -16,41 +17,41 @@ class ArenaTest {
 
         int[] fightResults = arena.fight(playerA.getAttack(), playerB.getStrength(), playerB.getHealth());
 
-        assertEquals(0, fightResults[0]); // Since attack is less than defend strength, damage should be 0
-        assertEquals(100, fightResults[1]); // Check if health reduction is correct
+        assertEquals(0, fightResults[0]);
+        assertEquals(100, fightResults[1]);
     }
 
     @Test
-    void attack() {
+    public void attack() {
         Game arena = new Arena();
 
         Player playerA = new Player("Player A", 50, 5, 10);
 
         int attackingPower = arena.attack(playerA.getAttack(), 4);
 
-        assertEquals(20, attackingPower); // Check if attacking power calculation is correct
+        assertEquals(40, attackingPower);
     }
 
     @Test
-    void defend() {
+    public void defend() {
         Game arena = new Arena();
 
         Player playerB = new Player("Player B", 100, 10, 5);
 
         int defendingStrength = arena.defend(playerB.getStrength(), 6);
 
-        assertEquals(60, defendingStrength); // Check if defending strength calculation is correct
+        assertEquals(60, defendingStrength);
     }
 
     @Test
-    void winnerDeclaration() {
+    public void winnerDeclaration() {
         Game arena = new Arena();
 
-        Player playerA = new Player("Player A", 0, 5, 10); // Player A has 0 health
+        Player playerA = new Player("Player A", 0, 5, 10);
         Player playerB = new Player("Player B", 100, 10, 5);
 
         Player winner = arena.winnerDeclaration(playerA, playerB);
 
-        assertEquals(playerB, winner); // Check if player B is declared as the winner
+        assertEquals(playerB, winner);
     }
 }
