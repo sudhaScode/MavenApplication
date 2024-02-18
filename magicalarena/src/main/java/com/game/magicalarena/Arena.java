2f35ca9632470c7ac1f3e6c89db4556d4c0ff4ab
sudhaScode <sunriser7995@gmail.com>
2024-02-18 14:36:26 +0530

    Unit testing

--- magicalarena/src/main/java/com/game/magicalarena/Arena.java
+++ magicalarena/src/main/java/com/game/magicalarena/Arena.java
@@ -1,14 +1,11 @@
 package com.game.magicalarena;
-
-import java.util.Random;
-
 import com.game.player.Player;
 
 public class Arena extends Game{
 
 	@Override
 	public int[] fight(int attackDamage, int defendStrength, int defenderHealth) {
-		// TODO Auto-generated method stub
+		
 		
 		int damaged = defendStrength<attackDamage?attackDamage-defendStrength:defendStrength-attackDamage;
 		int healthReduced = defenderHealth-damaged;
@@ -19,7 +16,7 @@ public class Arena extends Game{
 
 	@Override
 	public int attack(int attack, int diceValue) {
-		// TODO Auto-generated method stub
+		
 	
 		int attackingPower = attack*diceValue;
 		return attackingPower;
@@ -27,7 +24,7 @@ public class Arena extends Game{
 
 	@Override
 	public int defend(int strength, int diceValue) {
-		// TODO Auto-generated method stub
+		
 		
 		int defendingStrength = strength*diceValue;
 		return defendingStrength;
@@ -35,7 +32,7 @@ public class Arena extends Game{
 
 	@Override
 	public Player winnerDeclaration(Player A, Player B) {
-		// TODO Auto-generated method stub
+		
 		if(A.getHealth() == 0) {
 			return B;
 		}
---
+++ magicalarena/src/test/java/com/game/magicalarena/ArenaTest.java
@@ -0,0 +1,5 @@
+package com.game.magicalarena;
+
+public class ArenaTest {
+
+}
